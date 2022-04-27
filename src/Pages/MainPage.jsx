47ea832152810a95bc3f2f4a462b48b49180c715
tgandr <tgandr@gmail.com>
2022-04-27 19:01:48 -0300

    correções de lint e ajuste da seleção de categoria

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Categories from './Categories';
 import { Link } from 'react-router-dom';
+import Categories from './Categories';
 
 class MainPage extends React.Component {
   render() {
