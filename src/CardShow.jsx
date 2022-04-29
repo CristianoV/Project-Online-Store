95da83e8aae5705d17b6871ba1399a7474f4dda5
tgandr <tgandr@gmail.com>
2022-04-29 00:05:26 -0300

    oitavo requisito funcional mas não passa no teste: renderiza duas vezes o elemento do teste (livro O pequeno príncipe)

--- src/CardShow.jsx
+++ src/CardShow.jsx
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default class CardShow extends React.Component {
+  handleClick = (id) => {
+    const productSelected = [localStorage.getItem('cartItems')];
+    productSelected.push(id);
+    localStorage.setItem('cartItems', productSelected);
+    console.log(id);
+  }
+
   render() {
     const { title, thumbnail, price, id, information } = this.props;
     return (
@@ -21,6 +28,13 @@ export default class CardShow extends React.Component {
           <img src={ thumbnail } alt={ title } />
           <p>{ price }</p>
         </Link>
+        <button
+          type="submit"
+          onClick={ () => this.handleClick(id) }
+          data-testid="product-add-to-cart"
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
 
     );
--- src/Pages/ShopCart.jsx
+++ src/Pages/ShopCart.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import * as api from '../services/api';
 
 class ShopCart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+    };
+  }
+
+  componentDidMount() {
+    this.listProducts();
+  }
+
+  listProducts = () => {
+    let listItems = localStorage.getItem('cartItems');
+    listItems = listItems.split(',');
+    const items = [];
+    listItems.map(async (id) => {
+      const item = await api.getProductDetail(id);
+      items.push(item);
+      this.setState({ results: items });
+    });
+  }
+
   render() {
+    const { results } = this.state;
     return (
       <div>
-        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+        {results.length !== 0
+          ? (
+            <div>
+              {results.map((item) => (
+                <div
+                  key={ item.id }
+                  data-testid="shopping-cart-product-name"
+                >
+                  <p>{item.title}</p>
+                  <img src={ item.thumbnail } alt={ item.title } />
+                  <p>{ item.price }</p>
+                  <button type="button">
+                    +
+                  </button>
+                  <p data-testid="shopping-cart-product-quantity">1</p>
+                  <button type="button">
+                    -
+                  </button>
+                </div>
+              ))}
+            </div>)
+          : (<h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>)}
       </div>
     );
   }
