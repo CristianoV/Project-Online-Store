e6479dcc5c3738d6cd0bfbf89ce94c7cb1aa61ef
Cristiano Vieira <cristianoviieira@gmail.com>
2022-07-04 17:54:33 -0300

    Updates

---
+++ precache-manifest.298e524576bcdce62637276fdb1a174c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed30063647e19ba0522455fc8e0a5c55",
+    "url": "/Project-Online-Store/index.html"
+  },
+  {
+    "revision": "223292666da568dbdbfc",
+    "url": "/Project-Online-Store/static/css/main.58cfc718.chunk.css"
+  },
+  {
+    "revision": "c7707e7917da12635660",
+    "url": "/Project-Online-Store/static/js/2.9d48dcc6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Project-Online-Store/static/js/2.9d48dcc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223292666da568dbdbfc",
+    "url": "/Project-Online-Store/static/js/main.aa39b30e.chunk.js"
+  },
+  {
+    "revision": "439a3cdfdd3aff955fa7",
+    "url": "/Project-Online-Store/static/js/runtime-main.a2780d3d.js"
+  }
+]);
